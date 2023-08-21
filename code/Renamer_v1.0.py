83e1cc356abd7c99c0f2c6d07d821f06821aeb3a
Lonewolf239 <ocenkaigr@gmail.com>
2023-08-22 01:10:34 +0300

    Add files

---
+++ code/Renamer_v1.0.py
@@ -0,0 +1,19 @@
+import os
+
+print("|===========================|\n"
+      "|  Renamer by. Lonewolf239  |\n"
+      "|       VK: @1blitz01       |\n"
+      "|===========================|\n")
+folder_path = input("Enter the path to the folder in which you want to rename the files (example: C:\\\\test_folder)\n"
+                    "Folder path: ")
+files = os.listdir(folder_path)
+files.sort()
+for i, file_name in enumerate(files):
+    file_name_without_extension, extension = os.path.splitext(file_name)
+    new_file_name = f"{i + 1}{extension}"
+    os.rename(os.path.join(folder_path, file_name), os.path.join(folder_path, new_file_name))
+
+input("\n\n|==============================|\n"
+      "| The files have been renamed! |\n"
+      "|    Press enter to exit...    |\n"
+      "|==============================|")
